556d8398967904820e75cceb057db442bb83488b
Julien Christin <jul.christin@gmail.com>
2015-09-21 10:15:27 -0400

    De-activate apartment instead of move them into another collections

--- server.js
+++ server.js
@@ -18,16 +18,19 @@ if (process.env.NODE_ENV !== "production") {
 	log.err = console.log;
 }
 
-function removeApartment(id) {
-	database.collection("active").remove({
-		"_id": id
-	}, function(err) {
-		if (err) {
-			log.err(err);
-		} else {
-			log.info("Dead apartment: " + id);
+function removeApartment(apartment) {
+	apartment.active = false;
+	database.collection("apartments").update({
+			_id: apartment._id
+		}, apartment, {},
+		function(err) {
+			if (err) {
+				log.err(err);
+			} else {
+				log.info("Dead apartment: " + apartment._id);
+			}
 		}
-	});
+	);
 }
 
 function scrapApartment(apartment, update) {
@@ -38,12 +41,12 @@ function scrapApartment(apartment, update) {
 		if (error) {
 			log.err(error);
 		} else if (response.statusCode == 301) {
-			removeApartment(apartment._id);
+			removeApartment(apartment);
 		} else if (response.statusCode == 200) {
 			var $ = cheerio.load(body);
 
 			if ($("div.expired-ad-container").length > 0 || $("div.message-container").length > 0) {
-				removeApartment(apartment._id);
+				removeApartment(apartment);
 			} else {
 				var priceString = $("span[itemprop=price] strong").html().replace(/[\$,]/g, "");
 				apartment.latitude = $("meta[property='og:latitude']").attr("content");
@@ -51,6 +54,7 @@ function scrapApartment(apartment, update) {
 				apartment.image = $("img[itemprop=image]").attr("src");
 				apartment.price = parseInt(priceString);
 				apartment.last = new Date();
+				apartment.active = true;
 
 				var roomRegExpResult = /http:\/\/www\.kijiji\.ca\/.+-(\d)-1-2\//.exec(apartment._id);
 				apartment.room = roomRegExpResult ? parseInt(roomRegExpResult[1]) : undefined;
@@ -58,7 +62,7 @@ function scrapApartment(apartment, update) {
 				var date = $("table.ad-attributes tr:first-child td").html();
 				apartment.date = moment(date, "DD-MMM-YY").toDate();
 
-				database.collection("active").update({
+				database.collection("apartments").update({
 						_id: apartment._id
 					}, apartment, {
 						upsert: true
@@ -92,7 +96,7 @@ function checkForNewApartment() {
 			$("table.regular-ad").each(function(index, element) {
 				var url = "http://www.kijiji.ca" + $(element).attr("data-vip-url");
 
-				database.collection("active").findOne({
+				database.collection("apartments").findOne({
 					_id: url
 				}, {}, function(err, result) {
 					if (err) {
@@ -117,8 +121,9 @@ function checkForNewApartment() {
 }
 
 function updateLastApartment() {
-	database.collection("active").find({
-		source: "kijiji"
+	database.collection("apartments").find({
+		source: "kijiji",
+		active: true
 	}).sort({
 		"last": 1
 	}).limit(1).toArray(function(err, docs) {
